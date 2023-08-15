ec3c337e4906ea4de5521af0163ae0930338fee8
abylaykuat22 <abylaykuat@gmail.com>
2023-08-15 20:57:48 +0600

    sprint task 3-1

--- src/main/java/kz/bitlab/G115sprinttask31/controller/HomeController.java
+++ src/main/java/kz/bitlab/G115sprinttask31/controller/HomeController.java
@@ -6,7 +6,9 @@ import kz.bitlab.G115sprinttask31.models.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -26,6 +28,28 @@ public class HomeController {
   @PostMapping("/add-student")
   public String addStudent(Student student) {
     DBManager.addStudent(student);
+    return "redirect:/";
+  }
+
+  @GetMapping("/details/{id}")
+  public String detailsPage(@PathVariable Long id, Model model) {
+    Student student = DBManager.getStudentById(id);
+    model.addAttribute("student", student);
+    return "details";
+  }
+
+  @PostMapping("/edit-student")
+  public String editStudent(@RequestParam(name = "student_name") String name,
+      @RequestParam(name = "student_surname") String surname,
+      @RequestParam(name = "student_exam") int exam,
+      @RequestParam(name = "student_id") Long id) {
+    DBManager.editStudent(id, name, surname, exam);
+    return "redirect:/";
+  }
+
+  @PostMapping("/delete-student/{id}")
+  public String deleteStudent(@PathVariable Long id) {
+    DBManager.deleteById(id);
     return "redirect:/";
   }
 }
--- src/main/java/kz/bitlab/G115sprinttask31/db/DBManager.java
+++ src/main/java/kz/bitlab/G115sprinttask31/db/DBManager.java
@@ -2,6 +2,7 @@ package kz.bitlab.G115sprinttask31.db;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import kz.bitlab.G115sprinttask31.models.Student;
 import lombok.Getter;
 
@@ -10,28 +11,51 @@ public class DBManager {
   @Getter
   private static List<Student> students = new ArrayList<>();
 
-  private static Long id = 3L;
+  private static Long id = 4L;
 
   static {
     students.add(new Student(1L, "China", "China", 90, "A"));
     students.add(new Student(2L, "Danik", "Danik", 70, "B"));
-    students.add(new Student(2L, "Misha", "Misha", 60, "C"));
+    students.add(new Student(3L, "Misha", "Misha", 60, "C"));
   }
 
   public static void addStudent(Student student) {
     student.setId(id);
     id++;
-    if (student.getExam() >= 90) {
-      student.setMark("A");
-    } else if (student.getExam() >= 75) {
-      student.setMark("B");
-    } else if (student.getExam() >= 60) {
-      student.setMark("C");
-    } else if (student.getExam() >= 50) {
-      student.setMark("D");
-    } else {
-      student.setMark("F");
-    }
+    student.setMark(getMarkByExam(student.getExam()));
     students.add(student);
+  }
+
+  public static Student getStudentById(Long id) {
+    return students.stream()
+        .filter(student -> Objects.equals(student.getId(), id))
+        .findFirst()
+        .orElseThrow();
+  }
+
+  public static void editStudent(Long id, String name, String surname, int exam) {
+    Student student = getStudentById(id);
+    student.setName(name);
+    student.setSurname(surname);
+    student.setExam(exam);
+    student.setMark(getMarkByExam(exam));
+  }
+
+  private static String getMarkByExam(int exam) {
+    if (exam >= 90) {
+      return "A";
+    } else if (exam >= 75) {
+      return "B";
+    } else if (exam >= 60) {
+      return "C";
+    } else if (exam >= 50) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+
+  public static void deleteById(Long id) {
+    students.removeIf(student -> Objects.equals(student.getId(), id));
   }
 }
